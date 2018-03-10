c4907876177e3d91ff05ec1099cbdaef9aa096e1
Anuroop <anuroop.tblr@gmail.com>
2018-03-10 16:10:00 +0530

    Edit lambda

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ console.log('Loading function');
 
 exports.handler = (event, context, callback) => {
     // console.log('Received event:', JSON.stringify(event, null, 2));
-    callback(null, event);  // Echo back the first key value
+    // callback(null, event);  // Echo back the first key value
     //callback('Something went wrong');
+    console.log("yes")
 };
 
 
