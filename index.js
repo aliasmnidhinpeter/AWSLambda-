c0ea5a2671523a786045acbc6fc66946bed8f384
Anuroop <anuroop.tblr@gmail.com>
2018-03-10 16:14:32 +0530

    Edit lambda

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-'use strict';
+    'use strict';
 
-console.log('Loading function');
+    console.log('Loading function');
 
-exports.handler = (event, context, callback) => {
-    // console.log('Received event:', JSON.stringify(event, null, 2));
-    // callback(null, event);  // Echo back the first key value
-    //callback('Something went wrong');
-    console.log("yes")
-};
+    exports.handler = (event, context, callback) => {
+        // console.log('Received event:', JSON.stringify(event, null, 2));
+        // callback(null, event);  // Echo back the first key value
+        //callback('Something went wrong');
+        console.log("yes")
+    };
 
 
